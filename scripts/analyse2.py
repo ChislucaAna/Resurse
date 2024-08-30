aff7cdc34d6cef491744c653e865c6e4ccca1059
Andrei Piscoran <aosxap@gmail.com>
2024-08-30 14:27:50 +0300

    reformated and minor changes

--- scripts/analyse2.py
+++ scripts/analyse2.py
@@ -1,36 +1,35 @@
-from dotenv import load_dotenv
-
 import os
-import requests
-import tempfile
 import shutil
 import sys
+import tempfile
 
-from langchain_text_splitters import CharacterTextSplitter
-from langchain_community.document_loaders import TextLoader, PyPDFLoader, Docx2txtLoader
-from langchain_chroma import Chroma
-from langchain_community.llms import Ollama
-
-from transformers import AutoTokenizer, AutoModel
+import requests
 import torch
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import Docx2txtLoader, PyPDFLoader, TextLoader
+from langchain_community.llms import Ollama
+from langchain_text_splitters import CharacterTextSplitter
 from sklearn.metrics.pairwise import cosine_similarity
-
+from transformers import AutoModel, AutoTokenizer
 
 # Initialize environment variables
 load_dotenv()
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 persistent_directory = os.path.join(current_dir, "db", "chroma_db")
-input_file_path = os.path.join("/", "resources", sys.argv[1]) # name of the file
+input_file_path = os.path.join(
+    os.path.dirname(current_dir), "resources", sys.argv[1]
+)  # name of the file
 
 # Determine the file type based on the extension
 file_extension = os.path.splitext(input_file_path)[1].lower()
 
-if file_extension == '.pdf':
+if file_extension == ".pdf":
     # Use PyPDFLoader to load and parse the PDF file
     loader = PyPDFLoader(input_file_path)
     documents = loader.load()
-elif file_extension in ['.doc', '.docx']:
+elif file_extension in [".doc", ".docx"]:
     # Use Docx2txtLoader to load and parse the Word document
     loader = Docx2txtLoader(input_file_path)
     documents = loader.load()
@@ -60,14 +59,13 @@ for i, doc in enumerate(docs):
     ... return the keywords one after another with commas on a single line
     """
 
-    
     ollama_base_url = os.getenv("ngrok_ollama_server")
     model_local = Ollama(base_url=ollama_base_url, model="phi3:14b")
 
     response = model_local(query)
 
     print("This is one response:", response)
-        
+
     # Process the text to extract keywords
     keywords = response.strip().split(", ")
 
@@ -82,6 +80,7 @@ print(final_keywords)
 tokenizer = AutoTokenizer.from_pretrained("allenai/scibert_scivocab_uncased")
 model = AutoModel.from_pretrained("allenai/scibert_scivocab_uncased")
 
+
 # function to get embeddings for a phrase
 def get_embedding(phrase):
     inputs = tokenizer(phrase, return_tensors="pt")
@@ -89,6 +88,7 @@ def get_embedding(phrase):
     # Get the mean of the embeddings across tokens
     embedding = outputs.last_hidden_state.mean(dim=1)
     return embedding
+
 
 answer = ""
 # embeddings for the fields
@@ -98,10 +98,14 @@ embedding2 = get_embedding("computer science")
 # keep only words related to the mathematics and computer science fields
 for phrase in final_keywords.split(", "):
     embedding = get_embedding(phrase)
-    similarity1 = cosine_similarity(embedding1.detach().numpy(), embedding.detach().numpy())
-    similarity2 = cosine_similarity(embedding2.detach().numpy(), embedding.detach().numpy())
+    similarity1 = cosine_similarity(
+        embedding1.detach().numpy(), embedding.detach().numpy()
+    )
+    similarity2 = cosine_similarity(
+        embedding2.detach().numpy(), embedding.detach().numpy()
+    )
     if similarity1[0][0] >= 0.6 or similarity2[0][0] >= 0.6:
-        answer += phrase + ", " 
+        answer += phrase + ", "
 
 
 print("\n--- Final Keywords ---")
