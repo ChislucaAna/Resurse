4844ff92cc57073a915142381505e8b35fdd2172
Andreea-Ioana Gheorghe <163008395+andreea-ghe@users.noreply.github.com>
2024-09-03 14:15:36 +0300

    Create mock.py

---
+++ scripts/mock.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import sys
+import tempfile
+
+from langchain_community.document_loaders import Docx2txtLoader, PyPDFLoader, TextLoader
+from dotenv import load_dotenv
+
+load_dotenv()
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+input_file_path = os.path.join(
+      os.path.dirname(current_dir), sys.argv[1]
+)
+
+loader = PyPDFLoader(input_file_path)
+documents = loader.load()
+print("yay")
